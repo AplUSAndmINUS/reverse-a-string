2c24c8ded0f6dfdfcf2ea1717050214f3a63d7c1
12157591+AplUSAndmINUS@users.noreply.github.com <terence.waters@imail.org>
2020-03-15 14:05:12 -0600

    added logic and styling to form

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,67 @@ function App() {
   const [state, setState] = React.useState({
     string: ''
   });
+  const [reverseStr, setReverseStr] = React.useState('');
 
-  const reverseString = () => {};
+  const reverseString = (event: any) => {
+    event.preventDefault();
+    let newArr = [];
+    let newStr = '';
+    for (let i = 0; i < state.string.length; i++) {
+      newArr.push(state.string.charAt(i));
+    }
+
+    newStr = newArr.reverse().join('');
+    setReverseStr(`Reversed String: ${newStr}`);
+  };
 
   return (
     <>
       <Jumbotron>
         <h1>Reverse a string</h1>
         <p>
-          Description: This program takes the string entered and reverses it using JavaScript.
+          Description: This program takes the string entered and reverses it
+          using JavaScript.
         </p>
-
-        <Container fluid p-3>
-          <Row>
-            <Col md='auto'>
-              <Form onSubmit={reverseString}>
-                <Form.Group controlId='formString'>
-                  <Form.Label>String</Form.Label>
-                  <Form.Control
-                    type='text'
-                    name='string'
-                    value={state.string}
-                  />
-                </Form.Group>
-                <Button variant='primary' type='submit'>
-                  Submit
-                </Button>
-                <Button variant='secondary' type='submit'>
-                  Clear
-                </Button>
-              </Form>
-            </Col>
-          </Row>
-        </Container>
       </Jumbotron>
+
+      <Container fluid p-3>
+        <Row p-25>
+          <Col md='auto'>
+            <Form onSubmit={reverseString}>
+              <Form.Group controlId='formString'>
+                <Form.Label>String</Form.Label>
+                <Form.Control
+                  type='text'
+                  name='string'
+                  value={state.string}
+                  onChange={(e: any) => setState({ string: e.target.value })}
+                />
+              </Form.Group>
+              <Button variant='primary' type='submit'>
+                Submit
+              </Button>{' '}
+              <Button
+                variant='outline-secondary'
+                type='reset'
+                onClick={() => {
+                  setState({
+                    string: ''
+                  });
+                    setReverseStr('');
+                }}
+              >
+                Clear
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+        <Row p-25>
+          <Col md='auto'>
+            <p>{reverseStr}</p>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
